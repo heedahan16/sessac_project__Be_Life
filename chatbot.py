f155da3380124b5591112b8b61bf2a4d910652a7
heedahan16 <devhee.handa@gmail.com>
2023-11-25 22:32:30 +0900

    panel css 변경

--- chatbot.py
+++ chatbot.py
@@ -1,4 +1,5 @@
 import os
+import re
 from openai import OpenAI
 from dotenv import load_dotenv
 import panel as pn
@@ -24,73 +25,82 @@ panels = []
 
 context = [ {'role':'system', 'content':"""
 너는 사용자에게 운동프로그램을 추천하기 위한 챗봇이야
-사용자에게 먼저 인사를 하고, 해당하는 질문 9개야
-너가 먼저 1번부터 질문을 차례대로 하고 질문에 맞는 답변을 받았을때만 다음 질문을 던져줘.
-해당하는 질문에 모든 답변을 받으면, 항목별로 구분해서 dataframe을 만들어서 보여줘.
-
+사용자에게 먼저 인사를 하고, 총 9개의 질문을 1개씩만 해줘. 
+질문에 대한 답변을 정확하게 받아야지만 다음 질문을 해야 해.
+답변을 9개 전부 받으면 dataframe을 만들어서 보여줘.
+             
 질문은 아래와 같아 :
-
-1. 거주하시는 구와 동을 알려주세요. (예: 관악구 봉천동)
-2. 연령대를 골라주세요. (1: 학생(초, 중, 고등), 2: 성인, 3: 노인)
-3. 운동 목표를 골라주세요. (1: 심폐지구력 향상, 2: 근력 향상, 3: 체형 교정, 4: 무관)
-4. 건강 상태를 골라주세요. (1: 디스크, 2: 관절 질환, 3: 혈압 질환, 4: 대사증후군, 5: 해당 없음)
-5. 선호 시간대를 골라주세요. (1: 새벽, 2: 오전, 3: 오후, 4: 저녁, 5: 무관)
-6. 선호 운동을 골라주세요. (1: 구기및라켓, 2: 레저, 3: 무도, 4: 무용, 5: 민속, 6: 재활, 7: 체력단련및생활운동)
-7. 선호 인원수를 골라주세요. (1: 5명 이하, 2: 5명 이상, 3: 무관)
-8. 선호 빈도를 골라주세요. (1: 주1회, 2: 주2회, 3: 주3회, 4: 주4회 이상)
-9. 중요항목을 선택해주세요. (1: 거주지, 2: 연령대, 3: 운동목표, 4: 건강상태, 5: 선호시간대, 6: 선호운동, 7: 선호인원수, 8: 선호빈도, 9: 무관)
-
+1. 연령대를 선택해주세요. (1: 학생(초, 중, 고등), 2: 성인, 3: 노인)
+2. 건강 상태를 선택해주세요. (1: 디스크, 2: 관절 질환, 3: 혈압 질환, 4: 대사증후군, 5: 해당 없음)
+3. 운동 목표를 선택해주세요. (1: 심폐지구력 향상, 2: 근력 향상, 3: 체형 교정, 4: 무관)
+4. 선호 지역을 입력해주세요. (예: 관악구)
+5. 선호 빈도를 선택해주세요. (1: 주1회, 2: 주2회, 3: 주3회, 4: 주4회 이상)
+6. 선호 시간대를 선택해주세요. (1: 새벽, 2: 오전, 3: 오후, 4: 저녁, 5: 무관)
+7. 선호 운동을 선택해주세요. (1: 구기및라켓, 2: 레저, 3: 무도, 4: 무용, 5: 민속, 6: 재활, 7: 체력단련및생활운동)
+8. 선호 인원수를 선택해주세요. (1: 5명 이하, 2: 5명 이상, 3: 무관)
+9. 운동 프로그램을 선택할 때 가장 중요하게 생각하시는 항목을 골라주세요. (1: 연령대, 2: 건강상태, 3: 운동목표, 4: 선호 지역, 5: 선호빈도, 6: 선호시간대, 7: 선호운동, 8: 선호인원수, 9: 무관)
 """} ]
 
 def collect_messages(_):
-    # 텍스트 입력 위젯에서 사용자 입력 가져오기
     prompt = inp.value_input
-    # 다음 입력에 대한 text input 지우기
     inp.value = ''
-    # 사용자 입력을 시스템 역할과 함께 대화 context에 추가
+
+    # # 각 상호작용마다 패널 목록 재설정
+    # panels.clear()
+
     context.append({'role':'system', 'content':f"{prompt}"})
-    # 현재 대화 내용을 기반으로 AI 응답 가져오기
     response = get_completion_from_messages(context)
-    # AI의 응답을 시스템 역할과 함께 대화 context에  추가
     context.append({'role':'system', 'content':f"{response}"})
-    # Be_Life 패널에 AI 응답 표시
-    panels.append(pn.Row('Be_Life:', pn.pane.Markdown(prompt, width=800, styles={'background-color': '#f0fcd4'})))
-    # User 패널에 사용자 입력 표시
-    panels.append(pn.Row('User:', pn.pane.Markdown(prompt, width=800, styles={'background-color': '##FFA98F'})))
-    # 패널들이 있는 열 반환
+
+    panels.append(
+    pn.Row('사용자: ', pn.pane.Markdown(prompt, width=700, margin=10, styles={'border': '4px solid #90AFFF', 'border-radius': '15px', 'padding': '5px', 'margin': '10px'}))
+)
+    panels.append(
+    pn.Row('Be_Life: ', pn.pane.Markdown(response, width=700, margin=10, styles={'border': '2px', 'border-radius': '15px', 'background-color':'#90AFFF', 'padding': '10px'}))
+)
+
     return pn.Column(*panels)
 
-    # if ' ' not in prompt or '동' not in prompt:
-    #     response = '"관악구 봉천동"과 같이 구와 동을 정확히 입력해주세요.'
-    #     panels.append(pn.Row('Be_Life:', pn.pane.Markdown(response, width=800, styles={'background-color': '#f0fcd4'})))
-    #     return pn.Column(*panels)
-    
+inp = pn.widgets.TextInput(value="안녕하세요", placeholder='답변을 입력해주세요', width_policy='max', sizing_mode='stretch_width',
+                           width=750)
+button_conversation = pn.widgets.Button(name="입력", width_policy='max', sizing_mode='stretch_width', width=150)
 
-    # context.append({'role': 'system', 'content': f"{prompt}"})
-    # response = get_completion_from_messages(context)
-    # context.append({'role':'system', 'content':f"{response}"})
-    # panels.append(
-    #     pn.Row('User:', pn.pane.Markdown(prompt, width=800, styles={'background-color': '#FF607F'})))
-    # panels.append(
-    #     pn.Row('Be_Life:', pn.pane.Markdown(response, width=800, styles={'background-color': '#18CCA8'})))
-
-    # return pn.Column(*panels)
-
-inp = pn.widgets.TextInput(value="안녕하세요", placeholder='답변을 입력해주세요')
-button_conversation = pn.widgets.Button(name="입력")
-
-# 버튼 클릭 시 입력 값을 초기화
 def on_button_click(event):
-    inp.value = ""  
+    inp.value = ""  # 버튼 클릭 시 입력 값을 초기화
 
 button_conversation.on_click(on_button_click)
 
 interactive_conversation = pn.bind(collect_messages, button_conversation)
 
+
+input_and_button_style = {
+    'position': 'fixed',
+    'bottom': '0',
+    'width': '100%',
+    'padding': '10px',
+    'background-color': '#FAEBD7',
+    'border-top': '1px solid #ddd',
+}
+
 dashboard = pn.Column(
-    inp,
-    pn.Row(button_conversation),
-    pn.panel(interactive_conversation, loading_indicator=True, height=300),
+    pn.panel(interactive_conversation,
+             loading_indicator=True,
+             height=600,
+             margin=50,
+             padding=50,
+             sizing_mode='stretch_both'),
+    pn.Row(inp,
+           button_conversation,
+           css_classes=['input-and-button'], styles=input_and_button_style, sizing_mode='stretch_width',
+           height=60)
 )
+
+# 전체 화면 모드일 때 특정 너비 설정
+dashboard[1][0].sizing_mode = 'stretch_width'
+dashboard[1][1].sizing_mode = 'stretch_width'
+
+# 특정
+dashboard[1][0].width_policy = 'max'
+dashboard[1][1].width_policy = 'max'
 
 dashboard.show()
